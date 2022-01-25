4145235e5dac648730b0bbdc3fb0e9a9a812ad56
Luis Gibrann González Franco <luis.lgonzalez@mercadolibre.com.mx>
2022-01-24 22:15:40 -0600

    Se usa Fragment para devolver comopnentes mas complejos

--- src/PrimerApp.js
+++ src/PrimerApp.js
@@ -1,7 +1,13 @@
-import React from "react";
 
 const PrimerApp = () => {
- return <h1>Primera App en React</h1>;
+    /* En este caso los diamatnes pueden o no contener el uso del componente Fragment de React, 
+       sin embargo es recomendable ponerlos vacios para evitar imports innecesarios. */
+    return (
+        <>
+            <h1>Primera App en React</h1>
+            <p>Usamos fragmentos</p>
+        </>
+    );
 }
 
 export default PrimerApp;
