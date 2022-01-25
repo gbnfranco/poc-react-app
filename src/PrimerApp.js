693998ad393f26dc59ca56b2c2c57560f8801744
Luis Gibrann González Franco <luis.lgonzalez@mercadolibre.com.mx>
2022-01-24 23:35:42 -0600

    Se agregan PorpTypes para definir requeridos y tipos

--- src/PrimerApp.js
+++ src/PrimerApp.js
@@ -1,12 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const PrimerApp = ({ datos = "El componente padre no envío la información"}) => {
+const PrimerApp = ({ datos }) => {
 
+    // console.warn("Probando warns")
     return (
         <>
             <h1>Primera App en React</h1>
             <p>{datos}</p>
         </>
     );
+}
+
+PrimerApp.propTypes = {
+    datos: PropTypes.string.isRequired
 }
 
 export default PrimerApp;
