29531d6be84335cacc8b89ae0c3d26001af7ea34
Luis Gibrann González Franco <luis.lgonzalez@mercadolibre.com.mx>
2022-01-25 09:55:34 -0600

    Se hace uso de defaultProps

--- src/PrimerApp.js
+++ src/PrimerApp.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PrimerApp = ({ datos }) => {
+const PrimerApp = ({ datos, opcional }) => {
 
     // console.warn("Probando warns")
     return (
         <>
             <h1>Primera App en React</h1>
             <p>{datos}</p>
+            <p>{opcional}</p>
         </>
     );
 }
 
 PrimerApp.propTypes = {
-    datos: PropTypes.string.isRequired
+    datos: PropTypes.string.isRequired,
+    opcional: PropTypes.string
+}
+
+PrimerApp.defaultProps = {
+    opcional: "Tomamos valor por defecto",
 }
 
 export default PrimerApp;
